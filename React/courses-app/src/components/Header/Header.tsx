914b1804e4f9633e63dfb1b66bdb2a18629da5af
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-09-04 21:51:01 +0300

    feat: incorporate new user-facing feature
    create Header.tsx

---
+++ React/courses-app/src/components/Header/Header.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react';
+import './Header.css';
+import Button from '../../common/Button/Button';
+import Logo from './components/Logo/Logo';
+
+import { LOGOUT_BUTTON_TEXT } from '../../constants.js';
+
+const Header:FC = () => {
+  const handleLogout = () => {
+    console.log('обробник виходу з системи');
+    //todo: додайте обробник виходу з системи тут
+  };
+
+  return (
+    <header className='header'>
+      <Logo />
+      <div className='header__container'>
+        <p className='header__text'>Dave</p>
+        <Button buttonText={LOGOUT_BUTTON_TEXT} onClick={handleLogout} />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
