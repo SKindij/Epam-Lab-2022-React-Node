ef0e98067002efa98f32d8059dae395f47af4dd8
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-09-05 15:05:41 +0300

    feat: implement new module
    create CourseCard.tsx

---
+++ React/courses-app/src/components/Courses/components/CourseCard/CourseCard.tsx
@@ -0,0 +1,49 @@
+import { FC } from 'react';
+
+import Button from '../../../../common/Button/Button';
+import { dateGenerator } from '../../../../helpers/dateGenerator';
+import { pipeDuration } from '../../../../helpers/pipeDuration';
+
+import { SHOW_BUTTON_TEXT } from '../../../../constants';
+
+import './CourseCard.css';
+
+interface CourseCardProps {
+  title:string;
+  duration:number;
+  creationDate:string;
+  description:string;
+  authors:string[];
+}
+
+const CourseCard:FC<CourseCardProps> = ({
+  title, duration, creationDate, description, authors,
+}) => {
+  return (
+    <li className='item__container'>
+      <div className='item__text'>
+        <div className='item__title'>{title}</div>
+        <div className='item__desc'>{description}</div>
+      </div>
+      <div className='item__description'>
+        <div className='item__field'>
+          <span className='item__field--bold'>Authors: </span>
+          {authors.join(', ')} {/* Виведення авторів як рядка */}
+        </div>
+        <div className='item__field'>
+          <span className='item__field--bold'>Duration: </span>
+          {pipeDuration(duration)}
+        </div>
+        <div className='item__field'>
+          <span className='item__field--bold'>Created: </span>
+          {dateGenerator(creationDate)}
+        </div>
+        <div className='item__btn'>
+          <Button buttonText={SHOW_BUTTON_TEXT} />
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default CourseCard;
