a1b9baafe201ee6e489d79b52f334d67dfd81887
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-09-06 15:47:07 +0300

    refactor: improve code organization and structure
    update Input.tsx

--- React/courses-app/src/common/Input/Input.tsx
+++ React/courses-app/src/common/Input/Input.tsx
@@ -1,24 +1,28 @@
-import { ChangeEventHandler } from 'react';
+// Input.tsx
+import { ChangeEvent, FC } from 'react';
 import './Input.css';
 
 // даний інтерфейс описує типи пропсів
 interface InputProps {
   labelText:string;
   placeholderText:string;
-  onChange:ChangeEventHandler<HTMLInputElement>;
-  type?:string;
+  // обробник, що очікує подію ChangeEvent та має доступ до HTMLInputElement
+  onChange: (event:ChangeEvent<HTMLInputElement>) => void;
+  type?: 'text' | 'password' | 'email' | 'number';
   value:string;
+  id?: string; // опціональний пропс для id
 }
 
-const Input:React.FC<InputProps> = ({
+const Input:FC<InputProps> = ({
     labelText, placeholderText, onChange,
-    type = 'text', value,
+    type = 'text', value, id
   }) => {
   return (
     <form className='form'>
-      <label htmlFor='input'>{labelText}</label>
+      <label htmlFor={id}>{labelText}</label>
       <input
         className='input' type={type} name='input'
+		id={id} // вказує на htmlFor
         onChange={onChange} value={value}
         placeholder={placeholderText}       
       />
