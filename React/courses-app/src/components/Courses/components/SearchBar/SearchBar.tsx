d2cf207af2bfa4b915b4875f2042e640014d693e
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-09-06 16:10:08 +0300

    refactor: improve code organization and structure
    update SearchBar.tsx

--- React/courses-app/src/components/Courses/components/SearchBar/SearchBar.tsx
+++ React/courses-app/src/components/Courses/components/SearchBar/SearchBar.tsx
@@ -1,27 +1,30 @@
+// SearchBar.tsx
 import { FC, useState, ChangeEvent } from 'react';
 import Input from '../../../../common/Input/Input';
 import Button from '../../../../common/Button/Button';
 
 import { SEARCH_BUTTON_TEXT } from '../../../../constants';
-
 import './SearchBar.css';
 
 // інтерфейс описує типи пропсів компонента
 interface SearchBarProps {
+// очікує пропс, який є функцією, що приймає рядок і не повертає значення
   onUpdateSearch: (term:string) => void;
 }
 
 const SearchBar:FC<SearchBarProps> = ({ onUpdateSearch }) => {
   // для створення стану term з типом string
   const [term, setTerm] = useState<string>('');
-  // Тип параметра event вказано для правильної типізації
+  // Обробник події onChange для поля вводу,
+  // який викликається при зміні вмісту поля вводу
   const onUpdateInput = (event:ChangeEvent<HTMLInputElement>) => {
     const newTerm = event.target.value;
     setTerm(newTerm);
     if (newTerm.length === 0) onUpdateSearch(newTerm);
   };
-
+  // Обробник події для кнопки "Пошук":
   const onUpdateSearchLocal = () => {
+    // викликає функцію, передаючи поточний стан term
     onUpdateSearch(term);
   };
 
@@ -29,7 +32,7 @@ const SearchBar:FC<SearchBarProps> = ({ onUpdateSearch }) => {
     <div className='search-container'>
       <Input
         placeholderText={'Enter course name...'}
-        onChange={(event) => onUpdateInput(event)}
+        onChange={(event:ChangeEvent<HTMLInputElement>) => onUpdateInput(event)}
         value={term}
       />
       <Button buttonText={SEARCH_BUTTON_TEXT} onClick={onUpdateSearchLocal} />
@@ -37,4 +40,7 @@ const SearchBar:FC<SearchBarProps> = ({ onUpdateSearch }) => {
   );
 };
 
+// компонент SearchBar дозволяє користувачеві вводити пошуковий термін, 
+// обробляє зміни вводу, і передає цей термін до функції onUpdateSearch, 
+// коли користувач натискає кнопку "Пошук".
 export default SearchBar;
