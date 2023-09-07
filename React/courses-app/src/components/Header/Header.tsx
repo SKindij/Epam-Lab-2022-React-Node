7fe67091fce8056fc945ac3a4cfe5f05c43fb9e5
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-09-07 17:07:18 +0300

    feat: introduce user authentication feature
    update Header.tsx

--- React/courses-app/src/components/Header/Header.tsx
+++ React/courses-app/src/components/Header/Header.tsx
@@ -5,22 +5,30 @@ import Button from '../../common/Button/Button';
 import Logo from './components/Logo/Logo';
 
 import { LOGOUT_BUTTON_TEXT } from '../../constants';
-
+import { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+// визначаємо компонент, який приймає пропси типу FC (функційний компонент)
 const Header:FC = () => {
-  const handleLogout = () => {
-    console.log('обробник виходу з системи');
-    //todo: додайте обробник виходу з системи тут
-  };
+  // дозволяє отримати доступ до даних та функцій, доступних через цей контекст
+  const auth = useContext(AuthContext);
+  // функція для виходу користувача з системи
+  const logout = () => {
+		auth.logout();
+	};
 
+  // умовний оператор визначає, чи користувач аутентифікований
   return (
     <header className='header'>
       <Logo />
-      <div className='header__container'>
-        <p className='header__text'>Dave</p>
-        <Button buttonText={LOGOUT_BUTTON_TEXT} onClick={handleLogout} />
-      </div>
+	  {auth.isAuthenticated ? (
+        <div className='header__container'>
+          <p className='header__text'>{auth.username}</p>
+          <Button buttonText={LOGOUT_BUTTON_TEXT} onClick={logout} />
+        </div>
+	  ) : null}
     </header>
   );
 };
-
+// компонент відображає заголовок сайту з Logo та інфо користувача і кнопкою виходу, якщо користувач аутентифікований
+// це залежить від даних, які надаються через контекст AuthContext
 export default Header;
