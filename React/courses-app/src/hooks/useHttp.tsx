e86558d1310819da4e37dafc61165023163a92ec
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-09-08 08:45:26 +0300

    feat: add entirely new capabilities
    сreate useHttp.tsx

---
+++ React/courses-app/src/hooks/useHttp.tsx
@@ -0,0 +1,61 @@
+// useHttp.tsx
+import { useCallback, useState } from 'react';
+
+// оголошення типу для пропсів, які можна передавати в функцію request
+interface UseHttpProps {
+  url:string;
+  method?:string;
+  body?:any;
+  // означає, що headers повинен бути об'єктом (Record), 
+  // де ключі та значення повинні бути рядками
+  headers?:Record<string, string>;
+}
+// оголошення типів для повертаємих значень з функції useHttp
+interface UseHttpReturn {
+  loading:boolean;
+  error:any;
+  request: (props:UseHttpProps) => Promise<any>;
+}
+
+// функція, яка повертає об'єкт зі станами loading, error, та функцією request
+export const useHttp:()=>UseHttpReturn = () => {
+  // стан, що вказує, чи триває запит
+  const [loading, setLoading] = useState(false);
+  // стан, який містить помилки, якщо вони виникають під час запиту
+  const [error, setError] = useState<any>(null);
+  // функція для виконання HTTP-запитів
+  const request = useCallback(
+    async ({ url, method = 'GET', body, headers = {} }:UseHttpProps) => {
+      // встановлює стан true, щоб позначити початок запиту
+	  setLoading(true);
+      // якщо передано тіло запиту, встановлюється заголовок
+      if (body) {
+        headers['Content-Type'] = 'application/json';
+      }
+      // виконується фактичний HTTP-запит за допомогою fetch
+      try {
+        const response = await fetch(url, {
+          method,
+          body: body && JSON.stringify(body),
+          headers,
+        });
+        const data = await response.json();
+        setError(null);
+
+        if (!response.ok) {
+          throw new Error(data.errors || 'Something went wrong');
+        }
+        setLoading(false);
+        return data;
+      } catch (error) {
+        setLoading(false);
+        setError(error);
+        throw error;
+      }
+    },
+    []
+  );
+  // повертає об'єкт, який можна використовувати в інших компонентах 
+  // для виконання HTTP-запитів та обробки результатів
+  return { loading, error, request };
+};
