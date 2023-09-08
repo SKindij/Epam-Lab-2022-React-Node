ce4a73e6922143491a9adb30c776ba0464e38d57
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-09-08 09:40:52 +0300

    docs: clarify descriptions of key functionalities
    update useHttp.tsx

--- React/courses-app/src/hooks/useHttp.tsx
+++ React/courses-app/src/hooks/useHttp.tsx
@@ -23,7 +23,8 @@ export const useHttp:()=>UseHttpReturn = () => {
   const [loading, setLoading] = useState(false);
   // стан, який містить помилки, якщо вони виникають під час запиту
   const [error, setError] = useState<any>(null);
-  // функція для виконання HTTP-запитів
+  // request - функція для виконання HTTP-запитів
+  // useCallback використовується для забезпечення мемоізації функції, 
   const request = useCallback(
     async ({ url, method = 'GET', body, headers = {} }:UseHttpProps) => {
       // встановлює стан true, щоб позначити початок запиту
@@ -31,21 +32,25 @@ export const useHttp:()=>UseHttpReturn = () => {
       // якщо передано тіло запиту, встановлюється заголовок
       if (body) {
         headers['Content-Type'] = 'application/json';
-      }
-      // виконується фактичний HTTP-запит за допомогою fetch
+      }     
       try {
+	  // виконується фактичний HTTP-запит за допомогою fetch
         const response = await fetch(url, {
           method,
           body: body && JSON.stringify(body),
           headers,
         });
+		// після отримання відповіді витягуємо дані у форматі JSON 
         const data = await response.json();
         setError(null);
-
+        // перевіряється статус відповіді (чи вона успішна)
         if (!response.ok) {
+		  // помилка буде передана на зовнішній рівень
           throw new Error(data.errors || 'Something went wrong');
         }
+		// щоб позначити завершення запиту
         setLoading(false);
+		// дані, отримані з сервера, повертаються в request
         return data;
       } catch (error) {
         setLoading(false);
@@ -53,6 +58,8 @@ export const useHttp:()=>UseHttpReturn = () => {
         throw error;
       }
     },
+	// функція request не залежить від жодних змінних, 
+	// і вона буде однаковою після кожного рендеру компонента
     []
   );
   // повертає об'єкт, який можна використовувати в інших компонентах 
