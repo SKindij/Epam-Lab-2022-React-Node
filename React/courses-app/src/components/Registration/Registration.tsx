dea6e5b4fcda23f04260884ff58cf4f1dfa96c75
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-09-08 21:00:01 +0300

    docs: clarify descriptions of key functionalities
    update Registration.tsx

--- React/courses-app/src/components/Registration/Registration.tsx
+++ React/courses-app/src/components/Registration/Registration.tsx
@@ -1,5 +1,5 @@
 // Registration.tsx
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState, FC, ChangeEvent, FocusEvent, FormEvent } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 import Button from '../../common/Button/Button';
@@ -9,26 +9,35 @@ import { useHttp } from '../../hooks/useHttp';
 
 import './Registration.css';
 
-const Registration: React.FC = () => {
+const Registration:FC = () => {
   const { loading, error, request } = useHttp();
+  // стан для зберігання повідомлення про помилку під час реєстрації
   const [message, setMessage] = useState<string | null>(null);
+  // стани для зберігання значень полів форми
   const [name, setName] = useState<string>('');
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  // стани для відстеження, чи було поле змінено
   const [nameDirty, setNameDirty] = useState<boolean>(false);
   const [emailDirty, setEmailDirty] = useState<boolean>(false);
   const [passwordDirty, setPasswordDirty] = useState<boolean>(false);
+  // для зберігання повідомлень про помилки для кожного поля форми
   const [nameError, setNameError] = useState<string>('Name should not be empty');
   const [emailError, setEmailError] = useState<string>('Email should not be empty');
   const [passwordError, setPasswordError] = useState<string>('Password should not be empty');
+  // стан, що показує, чи є форма валідною
   const [formValid, setFormValid] = useState<boolean>(false);
-
+  
+  // за допомогою хука отримуємо функцію для переходу між сторінками
   const navigate = useNavigate();
-
+  
+  // для слідкування за змінами error та встановлення повідомлення про помилку
   useEffect(() => {
+    // якщо error існує, то присвоюється значення message, інакше встановлюється null
     setMessage(error?.message || null);
   }, [error]);
-
+  
+  // для визначення валідності форми на основі значень
   useEffect(() => {
     if (nameError || emailError || passwordError) {
       setFormValid(false);
@@ -37,18 +46,22 @@ const Registration: React.FC = () => {
     }
   }, [nameError, emailError, passwordError]);
 
-  const onNameChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+  // функції, які викликаються при зміні значень відповідних полів форми
+  // також перевіряють валідність введених даних і встановлюють повідомлення про помилку
+  const onNameChanged = (e:ChangeEvent<HTMLInputElement>) => {
     setName(e.target.value);
-    if (name.length < 1) {
+    if (name.length < 2) {
       setNameError('Name should be more than 2 characters');
     } else {
       setNameError('');
     }
   };
-
-  const onEmailChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+  // функція отримує об'єкт події e, який представляє зміни в полі введення <input>
+  const onEmailChanged = (e:ChangeEvent<HTMLInputElement>) => {
+    // значення, введене користувачем, присвоюється змінній email
     setEmail(e.target.value);
     const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+	// перевірка введеного email на відповідність регулярному виразу
     if (!reg.test(e.target.value.toLowerCase())) {
       setEmailError('Please, enter correct email');
     } else {
@@ -56,8 +69,11 @@ const Registration: React.FC = () => {
     }
   };
 
-  const onPasswordChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const onPasswordChanged = (e:ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
+	// будь-які символи можуть знаходитися між цифрами в паролі
+	// перевіряє наявність малих літер (a-z) 
+	// пароль повинен закінчуватися без пробілів
     const reg = /^(?=.*[0-9])(?=.*[a-z])(?=\S+$).{8,}$/;
     if (!reg.test(e.target.value.toLowerCase())) {
       setPasswordError(
@@ -67,9 +83,14 @@ const Registration: React.FC = () => {
       setPasswordError('');
     }
   };
-
-  const blurHandler = (e: React.FocusEvent<HTMLInputElement>) => {
+  // функція використовується для обробки події втрати фокусу (blur) на інпутах форми
+  const blurHandler = (e:FocusEvent<HTMLInputElement>) => {
+    // ключовий момент цієї функції - це використання e.target.name
+	// у браузері, коли відбувається подія втрати фокусу на полі вводу, 
+	// властивість name цього поля автоматично заповнюється іменем атрибута name, 
+	// який призначений для цього інпута в HTML-коді
     switch (e.target.name) {
+	  // дозволяє ідентифікувати, до якого поля відноситься подія втрати фокусу
       case 'name':
         setNameDirty(true);
         break;
@@ -84,15 +105,21 @@ const Registration: React.FC = () => {
     }
   };
 
-  const formSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  // функція викликається при подачі форми
+  // мета - відправити дані нового користувача на сервер для реєстрації
+  const formSubmit = async (event:FormEvent<HTMLFormElement>) => {
+    //  запобігає стандартній поведінці браузера - перезавантаженню сторінку
     event.preventDefault();
+	// новий об'єкт містить дані, введені користувачем у формі
     const newUser = {
       name: name,
       email: email,
       password: password,
     };
     try {
+	  // асинхронна ф-ція для виконання HTTP-запитів на сервер
       await request('/register', 'POST', { ...newUser });
+	  // виконується перенаправлення на сторінку входу
       navigate('/login');
     } catch (e) {}
   };
@@ -106,24 +133,17 @@ const Registration: React.FC = () => {
         <form className='registration__form' onSubmit={formSubmit}>
           <div className='registration__input'>
             <Input
-              labelText='Name'
-              placeholderText='Enter name'
-              value={name}
-              name='name'
-              onChange={onNameChanged}
-              onBlur={blurHandler}
+              labelText='Name' placeholderText='Enter name'
+              value={name} name='name'
+              onChange={onNameChanged} onBlur={blurHandler}
             />
             {nameDirty && nameError && <div className='error'>{nameError}</div>}
           </div>
           <div className='registration__input '>
             <Input
-              labelText='Email'
-              placeholderText='Enter email'
-              type='email'
-              value={email}
-              name='email'
-              onChange={onEmailChanged}
-              onBlur={blurHandler}
+              labelText='Email' placeholderText='Enter email'
+              type='email' value={email} name='email'
+			  onChange={onEmailChanged} onBlur={blurHandler}
             />
             {emailDirty && emailError && (
               <div className='error'>{emailError}</div>
@@ -131,11 +151,8 @@ const Registration: React.FC = () => {
           </div>
           <div className='registration__input'>
             <Input
-              labelText='Password'
-              placeholderText='Enter password'
-              type='password'
-              value={password}
-              name='password'
+              labelText='Password' placeholderText='Enter password'
+              type='password' value={password} name='password'
               onChange={onPasswordChanged}
               onBlur={blurHandler}
             />
@@ -146,8 +163,7 @@ const Registration: React.FC = () => {
           <div className='registration__btn registration__text'>
             <Button
               disabled={!formValid || loading}
-              type='submit'
-              buttonText='Registration'
+              type='submit' buttonText='Registration'
             />
           </div>
           <div className='error'>{message}</div>
