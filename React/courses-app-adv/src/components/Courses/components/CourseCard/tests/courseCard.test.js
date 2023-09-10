fe20bcbcbdb3bdbee9fb70e27b245c3605b493f9
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-09-10 21:09:53 +0300

    test: create test suite for edge cases and error scenarios
    create courseCard.test.js

---
+++ React/courses-app-adv/src/components/Courses/components/CourseCard/tests/courseCard.test.js
@@ -0,0 +1,6 @@
+// courseCard.test.js
+// [1 point] - CourseCard should display title.
+// [1 point] - CourseCard should display description.
+// [1 point] - CourseCard should display duration in the correct format.
+// [1 point] - CourseCard should display authors list.
+// [1 point] - CourseCard should display created date in the correct format.
