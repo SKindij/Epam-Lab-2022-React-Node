778636a29df9ca5cf073a4675e592e2395896f0f
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-09-12 20:56:51 +0300

    test: create test suite for edge cases and error scenarios
    create courseForm.test.js

---
+++ React/courses-app-adv/src/components/CourseForm/tests/courseForm.test.js
@@ -0,0 +1,5 @@
+// courseForm.test.js
+// CourseForm should show authors lists (all and course authors).
+// CourseForm 'Create author' click button should call dispatch.
+// CourseForm 'Add author' button click should add an author to course authors list.
+// CourseForm 'Delete author' button click should delete an author from the course list.
