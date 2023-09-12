30ec7ea7ba6ea894b0c1e77220541256f23be0c8
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-09-12 20:42:43 +0300

    test: create test suite for edge cases and error scenarios
    create courses.test.js

---
+++ React/courses-app-adv/src/components/Courses/tests/courses.test.js
@@ -0,0 +1,4 @@
+// courses.test.js
+// [2 points] - Courses should display amount of CourseCard equal length of courses array.
+// [1 point] - Courses should display Empty container if courses array length is 0.
+// [2 points] - CourseForm should be showed after a click on a button "Add new course".
