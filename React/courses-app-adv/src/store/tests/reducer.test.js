eb090f1e043b182a83645ae117799eccc1159da2
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-09-13 17:38:16 +0300

    init: create an additional file
    create reducer.test.js

---
+++ React/courses-app-adv/src/store/tests/reducer.test.js
@@ -0,0 +1,5 @@
+// reducer.test.js
+
+// [1 point] - reducer should return the initial state.
+// [1 point] - reducer should handle SAVE_COURSE and returns new state.
+// [1 point] - reducer should handle GET_COURSES and returns new state
