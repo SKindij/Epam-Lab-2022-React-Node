890ed405213ff6a96dd3f573d471822e80cadee3
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-09-17 22:34:54 +0300

    chore: perform general project maintenance tasks
    update index.js

--- Node/HW-1/index.js
+++ Node/HW-1/index.js
@@ -1,6 +1,7 @@
 // index.js
 // для роботи з файловою системою
 const fs = require('fs');
+const path = require('path');
 // для створення веб-сервера
 const express = require('express');
 // для логування запитів
@@ -12,12 +13,16 @@ const morgan = require('morgan')
 const app = express();
 // підключаємо роутер для обробки запитів, пов'язаних з файлами
 const { filesRouter } = require('./filesRouter.js');
+// запускаємо сервер на порту 8080 або іншому, якщо потрібно
+const PORT = process.env.PORT || 8080;
+// створюємо потік для запису логів HTTP-запитів у файл
+const accessedLog = fs.createWriteStream(path.join(__dirname, 'requestLogs.log'), {flags: 'a'});
 
 // додаємо два middleware до нашого Express додатку
 // розбирає тіло запиту з JSON-формату та робить його доступним у вигляді об'єкта req.body
 app.use(express.json());
-// в консолі надає короткі записи для кожного запиту
-app.use(morgan('tiny'));
+// логи мають бути направлені у окремий файл
+app.use(morgan('combined', { stream: accessedLog }));
 
 /*
   Вказуємо додатку використовувати роутер "filesRouter" для обробки всіх запитів, 
@@ -26,15 +31,16 @@ app.use(morgan('tiny'));
 */
 app.use('/api/files', filesRouter);
 
-
 const start = async () => {
   try {
     // якщо папка "files" не існує, то створюємо її;
     if (!fs.existsSync('files')) {
       fs.mkdirSync('files');
     }
-    // запускаємо сервер на порту 8080
-    app.listen(8080);
+    // запускаємо сервер на порту PORT
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
   } catch (err) {
     console.error(`Error on server startup: ${err.message}`);
   }
@@ -48,5 +54,5 @@ app.use(errorHandler)
 
 function errorHandler (err, req, res, next) {
   console.error('err')
-  res.status(500).send({'message': 'Server error'});
+  res.status(err.status).send({'message': err.message});
 }
