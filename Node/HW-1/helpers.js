c44a27ab937d30f0e419ea2176395b9e16a1f0b4
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-09-17 23:32:14 +0300

    fix: patche bug in codebase
    update helpers.js

--- Node/HW-1/helpers.js
+++ Node/HW-1/helpers.js
@@ -2,55 +2,58 @@
 const fs = require('fs');
 const path = require('path');
 // визначення шляху до теки, де зберігаються файли
-const dirPath = path.dirname('./files/');
-// перевірка наявності файлу 'protectedFiles' перед читанням
-if (!fs.existsSync('protectedFiles.json')) {
-    fs.writeFileSync('protectedFiles.json', '[]', 'utf8');
+const dirPath = path.join(__dirname, 'files');
+// функція для читання файлу 'protectedFiles.json'
+function readProtectedFiles() {
+	try {
+	  const protectedFilesJSON = fs.readFileSync(path.join(__dirname, 'protectedFiles.json'), 'utf8');
+	  return JSON.parse(protectedFilesJSON);
+	} catch (error) {
+	  console.error('Error reading protectedFiles.json:', error.message);
+	  return [];
+	}
 }
-try {
-  // зчитування даних про захищені файли з файлу 'protectedFiles.json'
-  const protectedFilesJSON = fs.readFileSync('./protectedFiles.json', 'utf8');
-} catch (error) {
-  console.error('Error reading protectedFiles.json:', error.message);
+// ф-ція для запису даних у файл 'protectedFiles.json'
+function writeProtectedFiles(data) {
+	try {
+	  fs.writeFileSync(path.join(__dirname, 'protectedFiles.json'), JSON.stringify(data, null, 2), 'utf8');
+	} catch (error) {
+	  console.error('Error writing protectedFiles.json:', error.message);
+	}
 }
+
 // функція для додавання файлу до списку захищених файлів
 function addToProtectedFiles(req) {
-	const { filename, password } = req.body;
+  const { filename, password } = req.body;
+	// отримуємо список захищених файлів
+	const protectedFiles = readProtectedFiles();
     // створення об'єкта, що представляє новий файл для захисту
-	const file = { file: { filename, password } };
-    // читання попередніх даних про захищені файли
-	const previousData = JSON.parse(
-		fs.readFileSync('protectedFiles.json', 'utf8')
-	);
+	const file = { filename, password };
     // додавання нового файлу до списку захищених файлів
-	previousData.push(file);
+	protectedFiles.push(file);
     // запис оновленого списку в файл 'protectedFiles.json'
-	fs.writeFile(
-		'protectedFiles.json',
-		JSON.stringify(previousData),
-		'utf8',
-		() => {console.log('Added to protected files');}
-	);
+	writeProtectedFiles(protectedFiles);
+    console.log('Added to protected files');
 }
 // ф-ція перевірки пароля та повернення вмісту захищеного файлу, якщо пароль правильний
 function checkPassword(filename, password, res) {
+  // Отримуємо список захищених файлів
+  const protectedFiles = readProtectedFiles();
   // лог для відстеження операції
   console.log(`Checking password for file: ${filename}`);
   if (
 	JSON.parse(protectedFilesJSON).some(
-	  (item) =>
-		item.file.filename === filename && item.file.password === password
+	  (item) =>	item.filename === filename && item.password === password
 	)
   ) {
 	// лог для відстеження операції
 	console.log(`Password for file '${filename}' is correct`);
     // якщо пароль правильний, то читаємо захищений файл та повертаємо його
-	const content = fs
-	  .readFileSync(`${dirPath}/${filename}`)
-	  .toString();
-      // отримуємо дату завантаження та розширення файлу
-	  const uploadedDate = fs.statSync(`${dirPath}/${filename}`).birthtime;
-	  const extension = path.extname(filename).slice(1);
+	const filePath = path.join(dirPath, filename);
+	const content = fs.readFileSync(filePath, 'utf8');
+    // отримуємо дату завантаження та розширення файлу
+	const uploadedDate = fs.statSync(filePath).birthtime;
+    const extension = path.extname(filename).slice(1);
       // повертаємо успішну відповідь разом із даними про файл
 	  console.log(`Sending protected file: ${filename}`);
 	  return res.status(200).send({
