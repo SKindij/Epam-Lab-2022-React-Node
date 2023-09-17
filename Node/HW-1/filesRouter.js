94666fd1ba831f103d80c2a532fd1d287e369489
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-09-17 17:10:11 +0300

    feat: introduct new API endpoint
    update filesRouter.js

--- Node/HW-1/filesRouter.js
+++ Node/HW-1/filesRouter.js
@@ -1,15 +1,15 @@
 // filesRouter.js
 const express = require('express');
 const router = express.Router();
-const { createFile, getFiles, getFile } = require('./filesService.js');
+const { createFile, getFiles, getFile, editFile, deleteFile } = require('./filesService.js');
 
 router.post('/', createFile);
-
 router.get('/', getFiles);
-
 router.get('/:filename', getFile);
 
 // Other endpoints - put, delete.
+router.put('/:filename', editFile);
+router.delete('/:filename', deleteFile);
 
 module.exports = {
   filesRouter: router
