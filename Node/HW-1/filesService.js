44f168b9dafaaab3131edf9b9fbca00e0ad320ea
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-09-17 19:20:06 +0300

    feat: add significant enhancements
    update filesService.js

--- Node/HW-1/filesService.js
+++ Node/HW-1/filesService.js
@@ -1,33 +1,176 @@
+// filesService.js
 // requires...
-
+const fs = require('fs');
+const path = require('path');
+const {
+	addToProtectedFiles,
+	checkPassword,
+} = require('./helpers');
 // constants...
+// папка, в якій будуть зберігатися файли
+const FILE_DIR = './files/';
+const EXTENSIONS = ['.log', '.txt', '.json', '.yaml', '.xml', '.js'];
+const protectedFilesJSON = fs.readFileSync('protectedFiles.json', 'utf8');
 
 function createFile (req, res, next) {
-  // code to create the file.
+  // code to create the file
+  const { filename, content, password } = req.body;
+  // перевірка, чи передані обов'язкові параметри
+  if (!filename || !content) {
+    next({
+      status: 400,
+      message: "Both filename and content are required.",
+    });
+    return;
+  }
+  // визначаємо розширення файлу
+  const fileExt = path.extname(filename);
+  if (!EXTENSIONS.includes(fileExt)) {
+    next({
+      status: 400,
+      message: 'App supports log, txt, json, yaml, xml, js file extensions',
+    });
+    return;
+  }
 
-  res.status(200).send({ "message": "File created successfully" });
+  try {   
+    // перевірка, чи існує файл з таким іменем
+    const filePath = path.join(FILE_DIR, filename);
+    if (fs.existsSync(filePath)) {
+      next({
+        status: 400,
+        message: 'A file with this name already exists',
+      });
+      return;      
+    }
+    // перевірка, чи передано пароль, і додавання файлу до захищених файлів
+    if (password) {addToProtectedFiles(req);}
+    // зберігання файлу з вказаним ім'ям та вмістом
+    fs.writeFileSync(filePath, content);
+    res.status(200).send({ "message": "File created successfully" });
+  } catch (error) {
+    console.error(`Error creating file: ${error.message}`);
+    res.status(500).send({ message: 'Server error' });
+  }
 }
 
 function getFiles (req, res, next) {
-  // code to get all files.
+  // Your code to get all files.
+  fs.readdir(FILE_DIR, (err, files) => {
+    if (err) {
+      next({ status: 400, message: 'Client error' });
+      return;
+    }
 
-  res.status(200).send({
-    "message": "Success",
-    "files": ["notes.txt"] });
+    res.status(200).send({
+      message: 'Success',
+      files: files,
+    });
+  });
 }
 
 const getFile = (req, res, next) => {
-  // code to get all files.
+  try {
+    let filename = req.url.slice(1);
+    // перевірка, чи існує файл з таким іменем
+    if (!fileExists(filename)) {
+      next({
+        status: 400,
+        message: `No file with '${filename}' filename found`,
+      });
+      return;      
+    }
+    // перевірка, чи файл захищений і чи передано пароль
+    const isProtectedFile = JSON.parse(protectedFilesJSON).some(
+      (item) => item.file.filename === filename
+    );
+    if (isProtectedFile && !req.query.password) {
+      next({
+        status: 400,
+        message: `This is a protected file. Provide the password.`,
+      });
+      return;  
+    }
 
-  res.status(200).send({
-    "message": "Success",
-    "filename": "notes.txt",
-    "content": "1. Create GitLab project. 2. Push the homework. 3. Upload homework details to the Excel file",
-    "extension": "txt",
-    "uploadedDate": "2022-07-22T17:05:25Z"});
+    if (isProtectedFile) {
+      // перевірка пароля та надсилання відповіді з вмістом захищеного файлу
+      checkPassword(filename, req.query.password, res);
+    } else {
+      // отримання вмісту незахищеного файлу
+      fs.readFile(path.join(FILE_DIR, filename), 'utf-8', (err, data) => {
+        if (err) {
+          next({
+            status: 400,
+            message: `No file with '${filename}' filename found`,
+          });
+          return;
+        }
+
+        res.status(200).send({
+          message: 'Success',
+          filename: path.basename(path.join(FILE_DIR, filename)),
+          content: data,
+          extension: path.extname(filename).slice(1),
+          uploadedDate: fs.statSync(path.join(FILE_PATH, filename)).birthtime,
+        });
+      });
+    } 
+  } catch (err) {
+    console.log('Server error');
+    res.status(500).send({
+      message: 'Server error',
+    });
+  }  
 }
 
 // Other functions - editFile, deleteFile
+function editFile(req, res, next) {
+  let filename = req.url.slice(1);
+
+  if (!fs.existsSync(path.join(FILE_PATH, filename))) {
+    next({
+      status: 400,
+      message: `No file with '${filename}' filename found`,
+    });
+    return;
+  }
+
+  fs.appendFile(path.join(FILE_PATH, filename), updatedText, (err) => {
+    if (err) {
+      next({
+        status: 400,
+        message: 'Something went wrong',
+      });
+      return;
+    }
+
+    res.status(200).send({ message: 'All changes are saved successfully' });
+  });
+}
+
+function deleteFile(req, res, next) {
+  let filename = req.url.slice(1);
+
+  if (!fs.existsSync(path.join(FILE_PATH, filename))) {
+    next({
+      status: 400,
+      message: `No file with '${filename}' filename found`,
+    });
+    return;
+  }
+
+  fs.unlink(path.join(FILE_PATH, filename), (err) => {
+    if (err) {
+      next({
+        status: 400,
+        message: 'Something went wrong',
+      });
+      return;
+    }
+
+    res.status(200).send({ message: 'File deleted successfully' });
+  });
+}
 
 // path.extName('file.txt') ---> '.txt'
 // fs.writeFile ({ flag: 'a' }) ---> adds content to the file
@@ -35,5 +178,7 @@ const getFile = (req, res, next) => {
 module.exports = {
   createFile,
   getFiles,
-  getFile
+  getFile,
+  editFile,
+  deleteFile
 }
