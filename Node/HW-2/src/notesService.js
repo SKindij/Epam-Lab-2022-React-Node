d9eea1014a5c6608d580792c9ab1246a02fc9f4f
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-10-18 12:02:36 +0300

    style: enhance code comments
    update notesService.js

--- Node/HW-2/src/notesService.js
+++ Node/HW-2/src/notesService.js
@@ -27,16 +27,15 @@ function createNote(req, res, next) {
 
 // Функція для отримання списку нотаток
 function getNotes(req, res, next) {
+  // витягуємо параметри із запиту клієнта
   const {offset, limit} = req.query;
-
   try {
+	// шукає всі нотатки, які належать користувачеві в базі даних MongoDB
     Note.find({userId: req.user.userId}).then((result) => {
-      // if (result.length === 0) {
-      //  return res.status(400).send({message: `No notes to show`});
-      // }
+	  // обмежує кількість нотаток відповідно до значення offset або нуль
       result = result.slice(offset || 0);
       result.length = limit || result.length;
-
+      // відправляємо відповідь клієнту
       res.json({
         'offset': offset || 0,
         'limit': limit || 'no limit',
@@ -51,15 +50,19 @@ function getNotes(req, res, next) {
 
 // Функція для отримання окремої нотатки за ідентифікатором
 const getNote = (req, res, next) => {
+  // виклик в базі даних MongoDB шукає нотатку, яка відповідає двом критеріям
   try {
+	// userId повинен відповідати ідентифікатору користувача  з JWT-токена
+	// _id - це ідентифікатор нотатки
     Note.find({userId: req.user.userId, _id: req.params.id})
-        .then((result) => {
-          if (result.length === 0) {
-            return res.status(400)
-                .send({message: `No ${req.user.username} notes with this id`});
-          }
+	  // нотатка знаходиться в результаті
+      .then((result) => {
+        if (result.length === 0) {
+          return res.status(400)
+            .send({message: `No ${req.user.username} notes with this id`});
+        }
           res.json({note: result[0]});
-        });
+      });
   } catch (err) {
     if (err) throw err;
   }
@@ -68,12 +71,13 @@ const getNote = (req, res, next) => {
 // Функція для оновлення нотатки
 const updateNote = async (req, res, next) => {
   try {
+	// пробуємо знайти нотатку, яку користувач бажає оновити
     const note = await Note.find({userId: req.user.userId, _id: req.params.id});
-
+    // Параметр з тіла запиту, який містить новий текст для нотатки.
     const {text} = req.body;
-
+    // note[0] (перший результат знайденого запиту) оновлюється на новий текст
     if (text) note[0].text = text;
-
+    // зберігаємо оновлену нотатку в базі даних
     return note[0].save().then((saved) =>
       res.json({message: 'Success', note: saved}));
   } catch (err) {
@@ -103,15 +107,15 @@ const updateNoteStatus = async (req, res, next) => {
 const deleteNote = (req, res, next) =>{
   try {
     Note.findOneAndRemove({userId: req.user.userId, _id: req.params.id})
-        .then((note) => {
-          if (!note) {
-            return res.status(400)
-                .send({message: `No ${req.user.username} notes with this id`});
-          }
+      .then((note) => {
+        if (!note) {
+          return res.status(400)
+            .send({message: `No ${req.user.username} notes with this id`});
+        }
 
-          res.status(200)
-              .send({message: `Note ${req.params.id} successfully deleted`});
-        });
+        res.status(200)
+          .send({message: `Note ${req.params.id} successfully deleted`});
+      });
   } catch (err) {
     if (err) throw err;
   }
