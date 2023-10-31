a7d844e9f7a80d0d1e5025fa2e8d0a34c512e1a9
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-10-31 15:15:53 +0200

    init: create an additional file
    add passwordMiddleware.js

---
+++ Node/HW-3/backend/src/middlewares/passwordMiddleware.js
@@ -0,0 +1,17 @@
+//middlewares/passwordMiddleware.js
+const passwordMiddleware = async (req, res, next) => {
+  const { _id } = req.user
+  const { oldPassword, newPassword } = req.body
+
+  req.user = {
+    _id,
+    oldPassword,
+    newPassword
+  }
+
+  next()
+}
+
+module.exports = {
+  passwordMiddleware
+}
