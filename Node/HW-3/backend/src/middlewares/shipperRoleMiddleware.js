1cd9b8c7d3d775dce7704a98cb16373992be537c
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-10-31 15:17:32 +0200

    init: add necessary files
    add driverRoleMiddleware.js and shipperRoleMiddleware.js

---
+++ Node/HW-3/backend/src/middlewares/shipperRoleMiddleware.js
@@ -0,0 +1,14 @@
+//middlewares/shipperRoleMiddleware
+const shipperRoleMiddleware = async (req, res, next) => {
+  const { role } = req.user
+
+  if (role !== 'SHIPPER') {
+    throw Error('No access permission')
+  }
+
+  next()
+}
+
+module.exports = {
+  shipperRoleMiddleware
+}
